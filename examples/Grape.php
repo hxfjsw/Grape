a519bee741e3b9eda2d0863cd0985a9ee0a9e0c5
hxfjsw <hxfjsw@qq.com>
2016-04-17 15:33:50 +0800

    提示文件

--- examples/Grape.php
+++ examples/Grape.php
@@ -5,3 +5,18 @@
  * Date: 16/4/17
  * Time: 下午3:30
  */
+
+namespace Grape;
+
+abstract class MainServer
+{
+    abstract public function onStart();
+
+    abstract public function onClose();
+
+    abstract public function onConnection();
+
+    abstract public function onDisconnection();
+
+}
+
